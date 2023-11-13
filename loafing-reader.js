5b1f2e54df9b77b614beffeaf64097f17aa076d1
HanaYabuki <1847196775@qq.com>
2023-11-13 21:40:57 +0800

    之前改了iframe规则，但版本号忘了加了

--- loafing-reader.js
+++ loafing-reader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         摸鱼小说阅读器 Loafing-Reader
 // @namespace    hanayabuki-loafing-reader
-// @version      1.2
+// @version      1.3
 // @description  内嵌浏览器里用来上班摸鱼看小说
 // @author       HanaYabuki
 // @match        *://*/*
